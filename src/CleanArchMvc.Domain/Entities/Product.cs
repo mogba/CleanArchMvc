eec06188324642a5ffd6279063d36a639c0d693e
Gabriel Morais <56984939+mogba@users.noreply.github.com>
2021-11-10 22:13:05 -0300

    Created alternative way for validation

--- src/CleanArchMvc.Domain/Entities/Product.cs
+++ src/CleanArchMvc.Domain/Entities/Product.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using CleanArchMvc.Domain.Exceptions;
 
 namespace CleanArchMvc.Domain.Entities
@@ -65,7 +67,6 @@ namespace CleanArchMvc.Domain.Entities
             CategoryId = categoryId;
         }
 
-
         private void ValidateDomain(
             string name, 
             string description,
@@ -97,6 +98,43 @@ namespace CleanArchMvc.Domain.Entities
                 image.Length > 250,
                 "Invalid image. Image must be a maximum of 250 characters"
             );
+        }
+    }
+
+    public class ProductValidation
+    {
+        private List<(bool, string)> _validationDefinition;
+
+        public ProductValidation(Product product)
+        {
+            _validationDefinition = new List<(bool, string)>()
+            {
+                (
+                    string.IsNullOrWhiteSpace(product.Name) || product.Name.Length < 3, 
+                    "Invalid name. Name is required and must be a minimum of 3 characters."
+                ),
+                (
+                    string.IsNullOrWhiteSpace(product.Description) || product.Description.Length > 250, 
+                    "Invalid description. Description is required and must be a minimum of 5 characters."
+                ),
+                (
+                    product.Price < 0, 
+                    "Invalid price value. Price must be equal or greater than 0."
+                ),
+                (
+                    product.Stock < 0, 
+                    "Invalid stock value. Stock must be equal or greater than 0."
+                ),
+                (
+                    product.Image.Length > 250, 
+                    "Invalid image. Image must be a maximum of 250 characters"
+                )
+            };
+        }
+
+        public IEnumerable<string> Validate()
+        {
+            return _validationDefinition.Where(p => p.Item1).Select(p => p.Item2);
         }
     }
 }
