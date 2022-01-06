70dfbc2ea815a6f439a822efb71a7f8ec40ef11f
Gabriel Morais <56984939+mogba@users.noreply.github.com>
2022-01-05 23:26:36 -0300

    Add DB context and entities configuration for database creation on code-first approach

---
+++ src/CleanArchMvc.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Name).HasMaxLength(100).IsRequired();
+
+            builder.HasData(
+                new Category(1, "Material Escolar"), 
+                new Category(2, "Livros"), 
+                new Category(3, "Acessórios"));
+        }
+    }
+}
